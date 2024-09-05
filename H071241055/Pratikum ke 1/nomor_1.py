857cc0ee61785a59c8fc15d65f130fa5a9a9f546
HA141User <muhammadhairi0123@gmail.com>
2024-09-05 14:08:52 +0800

    Pratikum1

--- H071241055/Pratikum ke 1/nomor_1.py
+++ H071241055/Pratikum ke 1/nomor_1.py
@@ -5,5 +5,5 @@ pilihan = ['jual', 'tahan', 'beli']
 persentasi = ((harga_sekarang - harga_kemarin)/harga_kemarin)*100
 hasil = pilihan[(persentasi > -3) + (persentasi > 5)]
 
-print(f"Perubahan persentasi harga saham adalah : {persentasi:.3f} %")
+print(f"Perubahan persentasi harga saham adalah : {persentasi:.2f} %")
 print(f"Rekomendasi Investasi : {hasil}")
